e68367e7f355626a5d0ba4cd1dcf555a174b7e5c
Muhammad3993 <muhammadjon3993@gmail.com>
2024-08-14 16:18:48 +0500

    Project create has been success

---
+++ src/constants.ts
@@ -0,0 +1,50 @@
+export const currencyByRupee: Currency[] = [
+  {
+    name: 'DOLLAR',
+    value: 0.000079,
+    flag: '🇺🇸',
+    symbol: '$',
+  },
+  {
+    name: 'EURO',
+    value: 0.000072,
+    flag: '🇪🇺',
+    symbol: '€',
+  },
+  {
+    name: 'POUND',
+    value: 0.000062,
+    flag: '🇬🇧',
+    symbol: '£',
+  },
+  {
+    name: 'RUBEL',
+    value: 0.007,
+    flag: '🇷🇺',
+    symbol: '₽',
+  },
+  {
+    name: 'AUS DOLLAR',
+    value: 0.00012,
+    flag: '🇦🇺',
+    symbol: 'A$',
+  },
+  {
+    name: 'CAN DOLLAR',
+    value: 0.00011,
+    flag: '🇨🇦',
+    symbol: 'C$',
+  },
+  {
+    name: 'YEN',
+    value: 0.011631,
+    flag: '🇯🇵',
+    symbol: '¥',
+  },
+  {
+    name: 'DINAR',
+    value: 0.0000242,
+    flag: '🇰🇼',
+    symbol: 'KD',
+  },
+];
